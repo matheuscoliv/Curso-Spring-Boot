8b1a7c3e5b8833bfa3d03937312d261fd4298b2d
matheuscoliv <matheushenrll@outlook.com>
2026-06-08 15:18:01 -0300

    mapeamento de url e disparando requisicoes

--- api/src/main/java/med/voll/api/ApiApplication.java
+++ api/src/main/java/med/voll/api/ApiApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ApiApplication {
-
+	/*carrega o tomcat, controllers...*/
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
 	}
